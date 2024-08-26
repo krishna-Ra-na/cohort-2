1539b259d43b996b875f46b92eec8dda9fe5ffff
krishna Rana <krishanj123@gmail.com>
2024-08-26 09:50:06 +0530

    middleware-p-solved

--- assignments/week-3/01-middlewares/03-errorcount.js
+++ assignments/week-3/01-middlewares/03-errorcount.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 
 const app = express();
 let errorCount = 0;
@@ -10,17 +10,31 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
+// const countError = (err, req, res, next) => {
+//   res.status(404).send({});
+//   errorCount += 1;
+// }
+
+app.get("/user", function (req, res) {
   throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/errorCount', function(req, res) {
+app.get("/errorCount", function (req, res) {
   res.status(200).json({ errorCount });
 });
-
+// app.use(countError);
+// pervious i put this app.use before all route and test fail ...then i put it after all routes to catch all errors
+app.use((err, req, res, next) => {
+  if (err) {
+    errorCount++;
+    res.status(404).send("Not Found");
+  } else {
+    next();
+  }
+});
 module.exports = app;
