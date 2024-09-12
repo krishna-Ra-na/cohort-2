63516d71721834964b7ec7febc8cb894d2f139ae
krishna Rana <krishanj123@gmail.com>
2024-09-12 18:10:52 +0530

    week-3-04-mongo-with-jwt-auth-solution-done

---
+++ assignments/week-3/04-mongo-with-jwt-auth/config.js
@@ -0,0 +1,5 @@
+const JWT_SECRET = "krishna-secret";
+
+module.exports = {
+  JWT_SECRET,
+};
--- assignments/week-3/04-mongo-with-jwt-auth/routes/user.js
+++ assignments/week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,26 +1,88 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username,
+    password,
+  });
+  res.json({
+    message: "User created successfully",
+  });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.find({ username, password });
+  if (user) {
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.json({
+      token,
+    });
+  } else {
+    res.status(411).json({
+      message: "Incorrect email and pass",
+    });
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  // Implement fetching all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const courseId = req.params.courseId;
+  const username = req.username;
+  console.log("username-console-", username);
+
+  await User.updateOne(
+    {
+      username: username,
+    },
+    {
+      $push: {
+        purchasedCourses: courseId,
+      },
+    }
+  );
+  res.json({
+    message: "Purchase complete!",
+  });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.username,
+  });
+
+  console.log(user.purchasedCourses);
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+
+  res.json({
+    courses: courses,
+  });
 });
 
-module.exports = router
+module.exports = router;
